af523c9f52dd30ce1a0416726817a551d44d033e
tfide <110505922+tfide@users.noreply.github.com>
2022-09-21 20:40:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 console.log("rodando o sistema de gerenciamento de jogos");
+console.log("A meu amigo, vou fala nada pro ce não, aquilo que oce fez aquele dia é prigoso, tem que toma cuidado com essas coisa que ce faz, Mensagem de Diegovisk Vulkrat");
